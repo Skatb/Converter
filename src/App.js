1c7ab843a3397c7ab162d99b97a0e81577e83fe9
Shvets Bogdan <pressssss@gmail.com>
2023-01-11 03:03:16 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import Header from './components/header/Header'
-import Main from './components/main/Main'
+import Range from './components/range/Range'
+import Weight from './components/weight/Weight';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
+      <Router>
+        <div className="wrapper">
+          <Header />
+
+          <Routes>
+            <Route path="/range" element={<Range />} />
+            <Route path="/weight" element={<Weight />} />
+          </Routes>
+        </div>
+      </Router>
     </div>
   );
 }
---
+++ src/components/weight/Weight.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './weight.css'
+
+const Weight = () => {
+    return (
+        <div className="weight">
+            вес
+        </div>
+    );
+};
+
+export default Weight;
